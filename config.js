e5fa23d779acf886bf390a9d7a27b9365a021aa8
Dhaloi <69165505+Dhaloi@users.noreply.github.com>
2020-12-13 12:24:50 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  "token": "NjQ5NjY2MzAzMDQ4Njc5NDI0.XePjBw.NVToGPn7mmY1jY13vCmQHKemTZQ",
+  "sahip": "731130300616147065S",
+
+  "dashboard" : {
+    "oauthSecret": "fhbbpNekZukdu-ak3tNavh6_8dKauCA7", // This is the `client` secret in your bot application page.
+    "callbackURL": "https://lunex.glitch.me/callback",
+    "sessionSecret": "super-secret-session-thing",
+    "domain": "https://lunex.glitch.me/",
+    "port": 8000
+  },
+};
+
+module.exports = config;
